01e55aad1812c02dd60dfdcd8de676c1f125c631
benassiPb <benassipb@gmail.com>
2022-06-04 10:32:48 -0700

    set up MongoDB

---
+++ index.js
@@ -0,0 +1,38 @@
+//dependencies
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+//db imports
+const Post = require('./models/Post');
+const { MONGODB } = require('./config.js');
+
+
+
+const resolvers = {
+  Query: {
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
